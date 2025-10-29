f2776c128614d89045038c8943298c154d6b9628
Rhyan-18 <24-38062@g.batstate-u.edu.ph>
2025-10-29 21:00:07 +0800

    Add files via upload

---
+++ AdminModule.java
@@ -0,0 +1,5 @@
+public class AdminModule {
+    public void adminHomePage() {
+        System.out.println("Welcome to the Admin Home Page!");
+    }
+}
---
+++ StudentModule.java
@@ -0,0 +1,6 @@
+public class StudentModule {
+    public void studentReportPage() {
+        System.out.println("Welcome to the Student Report Page!");
+    }
+}
+
